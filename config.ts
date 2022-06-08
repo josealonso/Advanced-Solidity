1a6e08201dfc2042ea9deaaab4fee3223c6ff126
josealonso <jose.alonso.programmer@gmail.com>
2022-06-08 18:01:22 +0200

    Implemented simple test for the FlashLoan contract

--- config.ts
+++ config.ts
@@ -2,7 +2,8 @@
 // Mainnet DAI Address
 export const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
 // Random user's address that happens to have a lot of DAI on Polygon Mainnet
-export const DAI_WHALE = "0xD92B63D0E9F2CE9F77c32BfeB2C6fACd20989eB3";
+// export const DAI_WHALE = "0xD92B63D0E9F2CE9F77c32BfeB2C6fACd20989eB3";
+export const DAI_WHALE = "0x4a35582a710e1f4b2030a3f826da20bfb6703c09";
 
 // Mainnet Pool contract address
 export const POOL_ADDRESS_PROVIDER = "0xa97684ead0e402dc232d5a977953df7ecbab3cdb";
