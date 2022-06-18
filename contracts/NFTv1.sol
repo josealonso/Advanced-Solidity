ec9b18d99af985d81ce8a23d2fe035ba107d6723
josealonso <jose.alonso.programmer@gmail.com>
2022-06-18 19:15:01 +0200

    Changed name of the implementation contract of the proxy pattern

--- contracts/NFTv1.sol
+++ contracts/NFTv1.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract UpgradeableNFT is
+contract NFTv1 is
     Initializable,
     ERC721Upgradeable,
     UUPSUpgradeable,
